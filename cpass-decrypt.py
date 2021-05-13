c2b5e8e8a12d2c1d8c07c0d182f20a9199335d85
Thanasis Mitragkas <55363474+sAsPeCt488@users.noreply.github.com>
2021-05-13 15:36:33 +0300

    Added Type Hints, Colored Output

--- cpass-decrypt.py
+++ cpass-decrypt.py
@@ -1,23 +1,18 @@
-# cPassword Decryption Tool.
-# Copyright (C) 2021 athanasios.mitragkas@gmail.com
-#
-# This tool may be used for legal purposes only.  Users take full responsibility
-# for any actions performed using this tool.  The author accepts no liability
-# for damage caused by this tool.  If these terms are not acceptable to you, then
-# do not use this tool.
+#!/usr/bin/env python3
+# coding=utf-8
 
 from Crypto.Cipher import AES
 from base64 import b64decode
 from Crypto.Util.Padding import unpad
 import argparse
 
-def decrypt(cpassword):
-    iv = 16*b'\x00'
-    ct = bytes(b64decode(cpassword+'==='))
-    key = bytes.fromhex('4e9906e8fcb66cc9faf49310620ffee8f496e806cc057990209b09a433b66c1b')
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    pt = unpad(cipher.decrypt(ct), 32).decode('utf-8').replace('\x00', '')
-    return pt
+def decode(cpassword) -> str: 
+	iv = 16*b'\x00'
+	ct = bytes(b64decode(cpassword+'==='))
+	key = bytes.fromhex('4e9906e8fcb66cc9faf49310620ffee8f496e806cc057990209b09a433b66c1b')
+	cipher = AES.new(key, AES.MODE_CBC, iv)
+	pt = unpad(cipher.decrypt(ct), 32).decode('utf-8').replace('\x00', '')
+	return pt
 
 
 print("""
@@ -29,14 +24,15 @@ print("""
  \___\_|  \__,_|___/___/ |___/ \___|\___|_|   \__, | .__/ \__|
                                                __/ | |        
                                               |___/|_|        
-     Made By: @sAsPeCt, @wizzard_alfredo
+	 Made By: @sAsPeCt, @wizzard_alfredo
 """)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("cPassword")
 args = parser.parse_args()
+
 try:
-    decrypted = decrypt(args.cPassword)
-    print(f"[+] Decrypted Password: {decrypted}")
+	decrypted = decode(args.cPassword)
+	print(f"\n[\033[92m+\033[0m] \033[92m Decrypted Password:\033[0m {decrypted}")
 except:
-    print("[-] Invalid cPassword")
+	print("\n[\033[91m-\033[0m] \033[91m\033[0m Invalid cPassword!\033[0m")
