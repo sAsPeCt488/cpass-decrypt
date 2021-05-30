b1ecd48f647786b815b54d6faa33c768ca352397
Thanasis Mitragkas <55363474+sAsPeCt488@users.noreply.github.com>
2021-05-30 13:00:13 +0300

    Renamed decode function to prevent conflicts

--- cpass-decrypt.py
+++ cpass-decrypt.py
@@ -6,7 +6,7 @@ from base64 import b64decode
 from Crypto.Util.Padding import unpad
 import argparse
 
-def decode(cpassword) -> str: 
+def decrypt(cpassword) -> str: 
 	iv = 16*b'\x00'
 	ct = bytes(b64decode(cpassword+'==='))
 	key = bytes.fromhex('4e9906e8fcb66cc9faf49310620ffee8f496e806cc057990209b09a433b66c1b')
@@ -32,7 +32,7 @@ parser.add_argument("cPassword")
 args = parser.parse_args()
 
 try:
-	decrypted = decode(args.cPassword)
+	decrypted = decrypt(args.cPassword)
 	print(f"\n[\033[92m+\033[0m] \033[92m Decrypted Password:\033[0m {decrypted}")
 except:
 	print("\n[\033[91m-\033[0m] \033[91m\033[0m Invalid cPassword!\033[0m")
